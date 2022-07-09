f3d22d6213a535f54f1132ae13805cae16944454
Luukas <luukas.makila@kolumbus.fi>
2022-07-09 13:15:51 +0300

    added db configuration

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,30 @@
 const express = require("express")
 const cors = require("cors")
+const mongoose = require("mongoose")
 const router = require("./controllers/main")
+require('dotenv').config()
+
 
 // APP CONFIG
 const app = express()
 const PORT = process.env.PORT || 3001
+const MONGO_URI = process.env.MONGO_URI
 
 // MIDDLEWARES
 app.use(express.json())
 app.use(cors())
 
-//ENDPOINTS
+//DATABASE CONFIGURATION
+console.log("Connecting to MongoDB")
+mongoose.connect(MONGO_URI)
+  .then(res => {
+    console.log("Successfully connected to MongoDB")
+  })
+  .catch(err => {
+    console.log(`Connection to MongoDB failed with error: ${err}`)
+  })
+
+// API ENDPOINTS
 app.use("/api", router)
 
 // LISTENER
