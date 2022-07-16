a45af449ad10ba14ed0202a9226cdb61b3b79a1e
Luukas <luukas.makila@kolumbus.fi>
2022-07-16 12:49:50 +0300

    added station fetching from db

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,12 +1,22 @@
 // Since the application is so small and doesn't require
 // alot of routes im defining all of them here.
 
+const Journeys = require("../models/journeys")
+const Stations = require("../models/stations")
+
 const journey_page = async (req, res, next) => {
   res.status(201).json({"message": "in reality return the journies"})
 }
 
 const station_page = async (req, res, next) => {
-  res.status(201).json({"message": "in reality return the stations"})
+  // Query all stations
+  try {
+    const stations = await Stations.find({})
+    res.status(201).json({"stations": stations})
+  } catch (error) {
+    res.status(500).json({"message": "An error occurred while fetching station data",
+                          "error": error.message})
+  }
 }
 
 const single_station = async (req, res, next) => {
--- frontend/src/components/StationPage.js
+++ frontend/src/components/StationPage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
 
 const StationPage = () => {
+  const [stations, setStations] = useState([])
+
+  useEffect(() => {
+    // Fetch data only on initial page laod
+    const getStations = async () => {
+      const result = await axios.get("http://localhost:3001/api/station_page")
+      setStations(result.data.stations)
+    }
+    getStations()
+  }, [])
+
   return (
     <div>
-      <h1>This is the station page</h1>
+      {stations.map((station, idx) => (
+        <div key={idx}>
+          <p>{ station.Nimi }</p>
+        </div>
+      ))}
     </div>
   )
 }
