7e9f084e02f3e57eaeceb830a8cf91fac5fb514c
Luukas <luukas.makila@kolumbus.fi>
2022-07-17 14:35:08 +0300

    single station view calculations

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -4,6 +4,32 @@
 const Journeys = require("../models/journeys")
 const Stations = require("../models/stations")
 
+const getJourneys = async (type, stationName) => {
+  let result = []
+  if(type === "starting"){
+    console.log("counting starts")
+    result = await Journeys.find({
+      departureStationName: stationName
+    })
+    //console.log(result[0])
+  } else if(type === "ending"){
+    result = await Journeys.find({
+      returnStationName: stationName
+    })
+    //console.log(result[0])
+  }
+  return result
+}
+
+const calculateAverageDistance = async (journeys) => {
+  let totalDistance = 0
+  const length = journeys.length
+  for(let i = 0; i < length; i += 1){
+    totalDistance += parseInt(journeys[i].coveredDistance)
+  }
+  return totalDistance / length
+}
+
 const journey_page = async (req, res, next) => {
   res.status(201).json({"message": "in reality return the journies"})
 }
@@ -20,7 +46,29 @@ const station_page = async (req, res, next) => {
 }
 
 const single_station = async (req, res, next) => {
-  res.status(201).json({"message": "in reality return single station info"})
+
+  // Get the station info
+  const stationId = req.query.stationId
+  const station = await Stations.findById(stationId)
+
+  // Get the arrays of journeys starting / ending at the station
+  const startingJourneys = await getJourneys("starting", station.Nimi)
+  const endingJourneys = await getJourneys("ending", station.Nimi)
+
+  // Calculate average distances
+  const averageDistanceStarting = await calculateAverageDistance(startingJourneys)
+  const averageDistanceEnding = await calculateAverageDistance(endingJourneys)
+
+  const stationInfo = {
+    name: station.Nimi,
+    address: station.Adress,
+    startingJourneys: startingJourneys.length,
+    endingJourneys: endingJourneys.length,
+    averageDistanceCoveredStarting: averageDistanceStarting,
+    averageDistanceCoveredEnding: averageDistanceEnding
+  }
+
+  res.status(201).json({"info": stationInfo})
 }
 
 module.exports = {
